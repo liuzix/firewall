1d582d56f6b2626d1cb16c685c7b8268c9df066d
Zixiong Liu <lzx@uchicago.edu>
2017-02-26 18:46:04 -0600

    .

--- tests.c
+++ tests.c
@@ -64,32 +64,32 @@ int test_main () {
             printf("W=%d\n", w2[i]);
             strat = LOCK_FREE;
             lock_gen = NULL;
-            double t1 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 10, 0);
+            double t1 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 32, 0);
             printf("lockfree, n = %d, speedup = %lf\n", n2[j], t0 / t1);
 
             strat = HOMEQUEUE;
             lock_gen = new_explock;
-            double t2 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 10, 0);
+            double t2 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 32, 0);
             printf("homequeue, exp, n = %d, speedup = %lf\n", n2[j], t0 / t2);
 
             strat = HOMEQUEUE;
             lock_gen = new_taslock;
-            double t3 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 10, 0);
+            double t3 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 32, 0);
             printf("homequeue, tas, n = %d, speedup = %lf\n", n2[j], t0 / t3);
 
             strat = RANDOMQUEUE;
             lock_gen = new_explock;
-            double t4 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 10, 0);
+            double t4 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 32, 0);
             printf("randomqueue, exp, n = %d, speedup = %lf\n", n2[j], t0 / t4);
 
             strat = RANDOMQUEUE;
             lock_gen = new_taslock;
-            double t5 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 10, 0);
+            double t5 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 32, 0);
             printf("randomqueue, tas, n = %d, speedup = %lf\n", n2[j], t0 / t5);
 
             strat = AWESOME;
             lock_gen = new_explock;
-            double t6 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 10, 0);
+            double t6 = parallelFirewall(T/n2[j], n2[j], w2[i], 1, 32, 0);
             printf("awesome, exp, n = %d, speedup = %lf\n", n2[j], t0 / t6);
         }
     }
@@ -107,32 +107,32 @@ int test_main () {
             printf("W=%d\n", w2[i]);
             strat = LOCK_FREE;
             lock_gen = NULL;
-            double t1 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 10, 0);
+            double t1 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 32, 0);
             printf("lockfree, n = %d, speedup = %lf\n", n2[j], t0 / t1);
 
             strat = HOMEQUEUE;
             lock_gen = new_explock;
-            double t2 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 10, 0);
+            double t2 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 32, 0);
             printf("homequeue, exp, n = %d, speedup = %lf\n", n2[j], t0 / t2);
 
             strat = HOMEQUEUE;
             lock_gen = new_taslock;
-            double t3 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 10, 0);
+            double t3 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 32, 0);
             printf("homequeue, tas, n = %d, speedup = %lf\n", n2[j], t0 / t3);
 
             strat = RANDOMQUEUE;
             lock_gen = new_explock;
-            double t4 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 10, 0);
+            double t4 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 32, 0);
             printf("randomqueue, exp, n = %d, speedup = %lf\n", n2[j], t0 / t4);
 
             strat = RANDOMQUEUE;
             lock_gen = new_taslock;
-            double t5 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 10, 0);
+            double t5 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 32, 0);
             printf("randomqueue, tas, n = %d, speedup = %lf\n", n2[j], t0 / t5);
 
             strat = AWESOME;
             lock_gen = new_explock;
-            double t6 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 10, 0);
+            double t6 = parallelFirewall(T/n2[j], n2[j], w2[i], 0, 32, 0);
             printf("awesome, exp, n = %d, speedup = %lf\n", n2[j], t0 / t6);
         }
     }
